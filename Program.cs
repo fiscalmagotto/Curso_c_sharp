eb7d8c328344448acf6301300af21dbeb465f160
Luiz Felipe <fiscalmagotto@gmail.com>
2020-03-27 13:21:55 -0300

    Projeto iniciado mostrando digite um numero

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoTeste_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
